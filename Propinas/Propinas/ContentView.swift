a79be8e61d5619f3eee50a5c4546422b61ad83b0
alexiaruizq <53828050+alexiaruizq@users.noreply.github.com>
2021-08-30 17:30:33 -0700

    Add files via upload

---
+++ Propinas/Propinas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Propinas
+//
+//  Created by Alexia Ruiz Quiroz on 30/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Propinas/Propinas/PropinasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropinasApp.swift
+//  Propinas
+//
+//  Created by Alexia Ruiz Quiroz on 30/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PropinasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
